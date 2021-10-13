c590c54e317cf4a36fc77174c543a8731baddd99
Markus Sommer <markus@splork.de>
2021-10-13 14:43:16 +0200

    Evaluate accelerometer data

--- ttcloud/ttt/policy.py
+++ ttcloud/ttt/policy.py
@@ -1,7 +1,9 @@
 import json
 import time
 
-from typing import Tuple, Union
+from typing import Tuple, Union, Dict, List
+from collections import defaultdict
+from statistics import mean, stdev
 
 from dataclasses import dataclass
 
@@ -21,6 +23,7 @@ from ttt.util import compute_temperature, compute_battery_voltage
 
 
 rde = 1
+analysis_interval = "2d"
 
 
 @dataclass
@@ -48,7 +51,7 @@ class LocalDataPolicy(Policy):
         )
 
         data: ResultSet = self.influx_client.query(
-            'SELECT "ttt_voltage" FROM "power" WHERE time > now() - 2d'
+            f'SELECT "ttt_voltage" FROM "power" WHERE time > now() - {analysis_interval}'
         )
         times = []
         voltages = []
@@ -96,26 +99,73 @@ class LocalDataPolicy(Policy):
 
         return measurement_interval
 
-    def _evaluate_brightness(self, packet: DataPacket, timelim="2d") -> int:
+    def _evaluate_brightness(self, packet: DataPacket) -> int:
         # Welche Variable enthält dies?
         pass
 
-    # macht es überhaupt sinn dies so zu evaluieren? schließlich bleibt ein umgefallener baum eine anomalie, selbst wenn es 3 tage her ist
-    # kurzfristige schwankungen sind aber in der Regel ganz normal, stärkeres schwingen kann sowohl Wetter als auch Krankheitsbedingt vom Baum sein
-    # Hier müsste eigentlich ein "known good value" definiert werden.
-    def _evaluate_gravity(self, packet: DataPacket, timelim="2d") -> int:
-        # gravity_z_mean=gravity_z_mean,
-        # gravity_z_derivation=gravity_z_derivation,
-        # gravity_y_mean=gravity_y_mean,
-        # gravity_y_derivation=gravity_y_derivation,
-        # gravity_x_mean=gravity_x_mean,
-        # gravity_x_derivation=gravity_x_derivation,
-        pass
+    def _evaluate_position(
+        self, packet: DataPacket, means: Dict[str, List[int]]
+    ) -> bool:
+        mean_x = mean(means["x"])
+        stdev_x = stdev(means["x"])
+        mean_y = mean(means["y"])
+        stdev_y = stdev(means["y"])
+        mean_z = mean(means["z"])
+        stdev_z = stdev(means["z"])
+
+        x = packet.gravity_x_mean
+        y = packet.gravity_y_mean
+        z = packet.gravity_z_mean
+
+        return (
+            abs(x - mean_x) > stdev_x
+            or abs(y - mean_y) > stdev_y
+            or abs(z - mean_z) > stdev_z
+        )
+
+    def _evaluate_movement(
+        self, packet: DataPacket, derivs: Dict[str, List[int]]
+    ) -> bool:
+        mean_x = mean(derivs["x"])
+        stdev_x = stdev(derivs["x"])
+        mean_y = mean(derivs["y"])
+        stdev_y = stdev(derivs["y"])
+        mean_z = mean(derivs["z"])
+        stdev_z = stdev(derivs["z"])
+
+        x = packet.gravity_x_derivation
+        y = packet.gravity_y_derivation
+        z = packet.gravity_z_derivation
+
+        return (
+            abs(x - mean_x) > stdev_x
+            or abs(y - mean_y) > stdev_y
+            or abs(z - mean_z) > stdev_z
+        )
+
+    def _evaluate_gravity(self, packet: DataPacket) -> int:
+        means: Dict[str, List[int]] = defaultdict(list)
+        derivs: Dict[str, List[int]] = defaultdict(list)
+        data: ResultSet = self.influx_client.query(
+            f'SELECT "x_mean", "x_derivation", "y_mean", "y_derivation", "z_mean", "z_derivation" FROM "gravity" WHERE time > now() - {analysis_interval}'
+        )
+
+        for datapoint in data.get_points("gravity"):
+            means["x"].append(datapoint["x_mean"])
+            derivs["x"].append(datapoint["x_derivation"])
+            means["y"].append(datapoint["y_mean"])
+            derivs["y"].append(datapoint["y_derivation"])
+            means["z"].append(datapoint["z_mean"])
+            derivs["z"].append(datapoint["z_derivation"])
+
+        return self._evaluate_position(
+            packet=packet, means=means
+        ) or self._evaluate_movement(packet=packet, derivs=derivs)
 
     # return: Gesamtabweichung - Grenzwerte müssen festgelegt werden - Tuple: referenzabweichung, vorheizung delta, nachheizung delta
     # Idee: referenzabweichung am wichtigsten für umgebungswerte, vorheizung delta erkennt ob sensoren defekt, nachheizung delta ob baum tot
     # timelim sollte 1d, 2d und 5d sein, sollte in evaluate funktion mehrfach aufgerufen werden
-    def _evaluate_temperatures(self, packet: DataPacket, timelim="2d") -> Tuple[int, int, int]:
+    def _evaluate_temperatures(self, packet: DataPacket) -> Tuple[int, int, int]:
         temperature_reference_0 = compute_temperature(packet.temperature_reference[0])
         temperature_reference_1 = compute_temperature(packet.temperature_reference[1])
         temperature_heat_0 = compute_temperature(packet.temperature_heat[0])
@@ -125,10 +175,8 @@ class LocalDataPolicy(Policy):
 
         # Welcher Sensor speist "air_temperature"? erstmal weggelassen
 
-        params = {"timelim": timelim}
         data: ResultSet = self.influx_client.query(
-            'SELECT "ttt_reference_probe_cold","ttt_reference_probe_hot","ttt_heat_probe_cold","ttt_heat_probe_hot" FROM "stem_temperature" WHERE time > now() - $timelim',
-            params={"params": json.dumps(params)}
+            f'SELECT "ttt_reference_probe_cold","ttt_reference_probe_hot","ttt_heat_probe_cold","ttt_heat_probe_hot" FROM "stem_temperature" WHERE time > now() - {analysis_interval}'
         )
 
         x = 0
@@ -139,20 +187,34 @@ class LocalDataPolicy(Policy):
         for datapoint in data.get_points("stem_temperature"):
             x = x + 1
             sum_delta_heat_pre = sum_delta_heat_pre + abs(
-                datapoint["ttt_heat_probe_cold"] - datapoint["ttt_reference_probe_cold"])
+                datapoint["ttt_heat_probe_cold"] - datapoint["ttt_reference_probe_cold"]
+            )
             sum_delta_heat_post = sum_delta_heat_post + abs(
-                datapoint["ttt_heat_probe_hot"] - datapoint["ttt_reference_probe_hot"])
-            sum_heat_reference = sum_heat_reference + ((abs(datapoint["ttt_heat_probe_cold"]) + abs(
-                datapoint["ttt_reference_probe_hot"]) + abs(datapoint["ttt_reference_probe_cold"])) / 3)
+                datapoint["ttt_heat_probe_hot"] - datapoint["ttt_reference_probe_hot"]
+            )
+            sum_heat_reference = sum_heat_reference + (
+                (
+                    abs(datapoint["ttt_heat_probe_cold"])
+                    + abs(datapoint["ttt_reference_probe_hot"])
+                    + abs(datapoint["ttt_reference_probe_cold"])
+                )
+                / 3
+            )
 
-        abw_referenz = ((abs(temperature_heat_0) + abs(temperature_reference_0) + abs(temperature_reference_1)) / 3) - (
-                sum_heat_reference / x)
+        abw_referenz = (
+            (
+                abs(temperature_heat_0)
+                + abs(temperature_reference_0)
+                + abs(temperature_reference_1)
+            )
+            / 3
+        ) - (sum_heat_reference / x)
         abw_delta_heat_pre = delta_heat_pre - (sum_delta_heat_pre / x)
         abw_delta_heat_post = delta_heat_post - (sum_delta_heat_post / x)
-        return [abw_referenz, abw_delta_heat_pre, abw_delta_heat_post]
+        return abw_referenz, abw_delta_heat_pre, abw_delta_heat_post
 
     def evaluate(
-            self, packet: Union[DataPacket, DataPacket2]
+        self, packet: Union[DataPacket, DataPacket2]
     ) -> Tuple[bool, TTCommand1]:
         return True, TTCommand1(
             receiver_address=packet.sender_address,
