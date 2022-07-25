fd2bd0f9782fc95b1a582f02732f7ae6c6b0d3fe
Markus Sommer <msommer@informatik.uni-marburg.de>
2022-07-25 16:41:56 +0200

    Rework cli of putter-script

--- eval/putter.py
+++ eval/putter.py
@@ -5,14 +5,11 @@ import requests
 import pickle
 import influxdb as influx
 from datetime import datetime
-from sys import exit
 from typing import List, Tuple, Dict, Any, Set
 from ttt import packets
 from tqdm import tqdm
 
 
-TT_CLOUDS = ["C2030115", "C2030116", "C2030117", "C2030119", "C2030123"]
-UPLOAD_DATABASE = "historical"
 UPLOAD_BATCH_SIZE = 100000
 
 
@@ -100,10 +97,11 @@ def upload_single(
     influx_client: influx.InfluxDBClient,
     tt_packets: List[Tuple[int, packets.TTPacket]],
     ttcloud: str,
+    upload_database: str,
 ) -> None:
     print(f"Uploading Packets from {ttcloud}")
 
-    ttcloud_database = f"{UPLOAD_DATABASE}-{ttcloud}"
+    ttcloud_database = f"{upload_database}-{ttcloud}"
     influx_client.create_database(ttcloud_database)
 
     points: List[Dict[str, Any]] = []
@@ -124,7 +122,7 @@ def upload_single(
     with tqdm(total=len(points)) as pbar:
         index = 0
         while index + UPLOAD_BATCH_SIZE < len(points):
-            influx_client.switch_database(UPLOAD_DATABASE)
+            influx_client.switch_database(upload_database)
             influx_client.write_points(
                 points[index : index + 100000], time_precision="s"
             )
@@ -143,53 +141,115 @@ def upload_single(
 def upload(
     influx_client: influx.InfluxDBClient,
     tt_packets: List[Tuple[str, List[Tuple[int, packets.TTPacket]]]],
+    upload_database: str,
 ) -> None:
-    influx_client.create_database(UPLOAD_DATABASE)
+    influx_client.create_database(upload_database)
     for ttcloud, tt_packets in tqdm(tt_packets):
         upload_single(
-            influx_client=influx_client, ttcloud=ttcloud, tt_packets=tt_packets
+            influx_client=influx_client,
+            ttcloud=ttcloud,
+            tt_packets=tt_packets,
+            upload_database=upload_database,
         )
+
+
+def _cli_download(args: argparse.Namespace) -> None:
+    print("Downloading packets")
+    tt_packets = download(ttclouds=args.clouds, address=args.address)
+
+    print("Pickling packets")
+    with open(args.file, "wb") as f:
+        pickle.dump(tt_packets, f, pickle.HIGHEST_PROTOCOL)
+
+
+def _cli_upload(args: argparse.Namespace) -> None:
+    print("Unpickling packets")
+    with open(args.file, "rb") as f:
+        tt_packets: List[Tuple[str, List[Tuple[int, packets.TTPacket]]]] = pickle.load(
+            f
+        )
+
+    print("Uploading packets")
+    influx_client = influx.InfluxDBClient(host=args.address, port=args.port)
+    upload(influx_client, tt_packets, args.database)
+    influx_client.close()
+
+
+def _cli_transfer(args: argparse.Namespace) -> None:
+    print("Downloading packets")
+    tt_packets = download(ttclouds=args.clouds, address=args.tt_address)
+
+    print("Uploading packets")
+    influx_client = influx.InfluxDBClient(
+        host=args.influx_address, port=args.influx_port
+    )
+    upload(influx_client, tt_packets, args.database)
+    influx_client.close()
+
+
+def _cli_count(args: argparse.Namespace) -> None:
+    print("Unpickling packets")
+    with open(args.file, "rb") as f:
+        tt_packets: List[Tuple[str, List[Tuple[int, packets.TTPacket]]]] = pickle.load(
+            f
+        )
+
+    print("Counting")
+    total = 0
+    for _, pkts in tt_packets:
+        total += len(pkts)
+    print(f"Total number of packets: {total}")
 
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(
         description="Download historical data and do stuff with it"
     )
-    parser.add_argument(
+    subparsers = parser.add_subparsers()
+
+    command_download = subparsers.add_parser(
+        "download", help="Download data from TreeTalker-backend and write to disk"
+    )
+    command_download.add_argument(
         "-a", "--address", help="Address of the server hosting the data"
     )
-    parser.add_argument("action", help="One of [put, dump, upload, count]")
-    parser.add_argument(
-        "-p",
-        "--pickle",
-        help="If action is 'dump' or 'upload', filename for the pickled data",
+    command_download.add_argument("-f", "--file", help="Filename for data-file")
+    command_download.add_argument("-c", "--clouds", nargs="+", help="TTCloud IDs")
+    command_download.set_defaults(func=_cli_download)
+
+    command_upload = subparsers.add_parser(
+        "upload", help="Upload data from disk to influxdb"
     )
+    command_upload.add_argument(
+        "-a", "--address", help="Address of the influxdb", default="localhost"
+    )
+    command_upload.add_argument(
+        "-p", "--port", help="Port of the influxdb", type=int, default=8086
+    )
+    command_upload.add_argument("-f", "--file", help="Filename of data-file")
+    command_upload.add_argument("-d", "--database", help="Name of influxdb-database")
+    command_upload.set_defaults(func=_cli_upload)
+
+    command_transfer = subparsers.add_parser(
+        "transfer", help="Transfer data from TreeTalker-Backend to influxdb"
+    )
+    command_transfer.add_argument(
+        "-t", "--tt_address", help="Address of the server hosting the TreeTalker data"
+    )
+    command_transfer.add_argument(
+        "-i", "--influx_address", help="Address of the influxdb", default="localhost"
+    )
+    command_transfer.add_argument(
+        "-p", "--influx_port", help="Port of the influxdb", type=int, default=8086
+    )
+    command_transfer.add_argument("-d", "--database", help="Name of influxdb-database")
+    command_transfer.set_defaults(func=_cli_transfer)
+
+    command_count = subparsers.add_parser(
+        "count", help="Count data records in local data file"
+    )
+    command_count.add_argument("file", help="Filename of data-file")
+    command_count.set_defaults(func=_cli_count)
+
     args = parser.parse_args()
-
-    if args.action == "put" or args.action == "dump":
-        print("Downloading packets")
-        tt_packets = download(ttclouds=TT_CLOUDS, address=args.address)
-    elif args.action == "upload" or args.action == "count":
-        print("Unpickling packets")
-        with open(args.pickle, "rb") as f:
-            tt_packets: List[
-                Tuple[str, List[Tuple[int, packets.TTPacket]]]
-            ] = pickle.load(f)
-    else:
-        print(f"Unknown action '{args.action}'")
-        exit(1)
-
-    if args.action == "put" or args.action == "upload":
-        print("Uploading packets")
-        influx_client = influx.InfluxDBClient(host="localhost", port=8086)
-        upload(influx_client, tt_packets)
-        influx_client.close()
-    elif args.action == "dump":
-        print("Pickling packets")
-        with open(args.pickle, "wb") as f:
-            pickle.dump(tt_packets, f, pickle.HIGHEST_PROTOCOL)
-    elif args.action == "count":
-        total = 0
-        for _, pkts in tt_packets:
-            total += len(pkts)
-        print(f"Total number of packets: {total}")
+    args.func(args)
